c63e6d2bdf6ea2a6ee83a2c8a83d94211f78271d
Jordan <lippertx11@gmail.com>
2024-03-26 22:10:54 -0300

    conclusao teste2

--- TransformacaoDados.py
+++ TransformacaoDados.py
@@ -23,6 +23,20 @@ with ZipFile(os.path.join(pasta_destino, 'Anexos.zip'), 'r') as zip:
                 print("Arquivo CSV gerado com sucesso!")
             else: 
                 print("Erro: Os dados do arquivo nao sao um DataFrame")
+    #Substituição das abreviações OD e AMB            
+    df = pd.read_csv('Anexo_I.csv')
+    df = df.replace('OD', 'SEG. ODONTOLOGICA')
+    df = df.to_csv("Anexo_I_Editado1.csv", index=False)
+    df = pd.read_csv('Anexo_I_Editado1.csv')
+    df = df.replace('AMB', 'SEG. AMBULATORIAL')
+    df = df.to_csv("Anexo_I_Editado2.csv",  header=1, index=True)
 
+#Compactação do arquivo CSV modificado
+with ZipFile(os.path.join(pasta_destino, "Teste_Jordan_Lippert_de_Oliveira.zip"),'w') as zip:
+    zip.write("Anexo_I_Editado2.csv")
+
+#Removendo os arquivos que não serão usados
 os.remove('Anexo_I_Rol_2021RN_465.2021_RN599_RN600.pdf')
-    
+os.remove('Anexo_I.csv')
+os.remove('Anexo_I_Editado1.csv')
+os.remove('Anexo_I_Editado2.csv')
